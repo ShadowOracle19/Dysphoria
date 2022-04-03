13a4992d2a644091a51f7b0c07b8b5912ea475d1
Lucas Coates <coatesl@sheridancollege.ca>
2022-04-03 18:33:21 -0400

    updated dream world map

---
+++ Assets/C# scipt/Interactions/HideOrSit.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOrSit : MonoBehaviour, IInteractable
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string GetDescription()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+}
